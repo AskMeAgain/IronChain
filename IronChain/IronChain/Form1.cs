67738e0b7f7553aaa568ce6e4053975020ba5e72
Kelvin <test@test.com>
2017-08-13 21:17:45 +0200

    getting the utility working

--- IronChain/IronChain/Form1.cs
+++ IronChain/IronChain/Form1.cs
@@ -10,8 +10,42 @@ using System.Windows.Forms;
 
 namespace IronChain {
     public partial class Form1 : Form {
+
+        public static Form1 instance;
+
         public Form1() {
             InitializeComponent();
+            instance = this;
         }
+
+        private void onClickStoreFile(object sender, EventArgs e) {
+
+            Block b1 = new Block(3333);
+
+            Utility.storeFile(b1, "test");
+
+        }
+
+        private void onClickLoadFile(object sender, EventArgs e) {
+
+            Block b = Utility.loadFile<Block>("test");
+
+            if (b != null) {
+                addToLog("Block loaded: " + b.name);
+            } else {
+                addToLog("Failure loading block!");
+
+            }
+
+        }
+
+        public void addToLog(string s) {
+
+            String text = textBox1.Text;
+            text += s + Environment.NewLine;
+            textBox1.Text = @text;
+
+        }
+
     }
 }
---
+++ IronChain/IronChain/Utility.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace IronChain {
+    public static class Utility {
+
+        public static T loadFile<T>(string fileName) {
+            if (string.IsNullOrEmpty(fileName)) { return default(T); }
+
+            T objectOut = default(T);
+
+            try {
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(fileName);
+                string xmlString = xmlDocument.OuterXml;
+
+                using (StringReader read = new StringReader(xmlString)) {
+                    Type outType = typeof(T);
+
+                    XmlSerializer serializer = new XmlSerializer(outType);
+                    using (XmlReader reader = new XmlTextReader(read)) {
+                        objectOut = (T)serializer.Deserialize(reader);
+                        reader.Close();
+                    }
+
+                    read.Close();
+                }
+            } catch (Exception ex) {
+                //Log exception here
+            }
+
+            return objectOut;
+        }
+
+        public static void storeFile<T>(T serializableObject, string fileName) {
+            if (serializableObject == null) { return; }
+
+            try {
+                XmlDocument xmlDocument = new XmlDocument();
+                XmlSerializer serializer = new XmlSerializer(serializableObject.GetType());
+                using (MemoryStream stream = new MemoryStream()) {
+                    serializer.Serialize(stream, serializableObject);
+                    stream.Position = 0;
+                    xmlDocument.Load(stream);
+                    xmlDocument.Save(fileName);
+                    stream.Close();
+                    Form1.instance.addToLog("Storing file succesful");
+                }
+            } catch (Exception ex) {
+
+            }
+        }
+
+    }
+}
