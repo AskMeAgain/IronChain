baa9883187c8a94a649c187ccce48157e2e14925
Kelvin <test@test.com>
2017-08-31 00:14:35 +0200

    gui update 4

--- IronChain/IronChain/Form1.Designer.cs
+++ IronChain/IronChain/Form1.Designer.cs
@@ -57,11 +57,11 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.label10 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.label12 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -74,7 +74,7 @@
             this.textBox2.Location = new System.Drawing.Point(12, 158);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(270, 260);
+            this.textBox2.Size = new System.Drawing.Size(359, 260);
             this.textBox2.TabIndex = 4;
             // 
             // label3
@@ -379,25 +379,24 @@
             this.panel3.Size = new System.Drawing.Size(403, 141);
             this.panel3.TabIndex = 61;
             // 
-            // label10
+            // button3
             // 
-            this.label10.AutoSize = true;
-            this.label10.BackColor = System.Drawing.SystemColors.Control;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(-2, 0);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(128, 26);
-            this.label10.TabIndex = 0;
-            this.label10.Text = "Miner Panel";
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(35, 94);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 28);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Stop Mining";
+            this.button3.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // label12
             // 
-            this.button1.Location = new System.Drawing.Point(35, 60);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 28);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Start Mining";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(210, 41);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(74, 13);
+            this.label12.TabIndex = 3;
+            this.label12.Text = "Miner Address";
             // 
             // comboBox2
             // 
@@ -409,24 +408,25 @@
             this.comboBox2.Size = new System.Drawing.Size(137, 28);
             this.comboBox2.TabIndex = 2;
             // 
-            // label12
+            // button1
             // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(210, 41);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(74, 13);
-            this.label12.TabIndex = 3;
-            this.label12.Text = "Miner Address";
+            this.button1.Location = new System.Drawing.Point(35, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start Mining";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // label10
             // 
-            this.button3.Enabled = false;
-            this.button3.Location = new System.Drawing.Point(35, 94);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(139, 28);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Stop Mining";
-            this.button3.UseVisualStyleBackColor = true;
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.SystemColors.Control;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(-2, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(128, 26);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Miner Panel";
             // 
             // Form1
             // 
