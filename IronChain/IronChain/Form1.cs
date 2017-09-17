d21e750a69d8985bd728bf88e4d46b41f2b3da3b
Kelvin <test@test.com>
2017-09-17 20:49:16 +0200

    Revert "V1.2"
    This reverts commit a0791a66c776e05b7711ec24c70755439486f796.

--- IronChain/IronChain/Form1.cs
+++ IronChain/IronChain/Form1.cs
@@ -401,7 +401,7 @@ namespace IronChain {
             i--;
 
             if (errorFlag) {
-                Console.WriteLine("ERROR BLOCK");
+                Console.WriteLine("ERROR BLOCK<this?");
 
                 File.Delete(globalChainPath + i + ".blk");
                 File.Delete(globalChainPath + "P" + i + ".blk");
